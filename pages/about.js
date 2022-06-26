a50efc757de73c420b3d0aae85f14d785c043c1b
Shadrack Bakasa <shadrackbakasa5@gmail.com>
2022-06-26 19:50:55 +0300

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,33 @@
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Atque, qui quae culpa possimus veritatis aperiam eveniet
+                    laborum beatae! Ratione officiis perspiciatis iure. Ab, ex
+                    fuga! Nisi rem voluptates, quam sequi unde corrupti odit
+                    modi repudiandae. Aliquam doloremque esse unde accusantium
+                    similique, optio rem. Nam blanditiis consectetur voluptate
+                    nihil sint. Cumque!
+                </p>
+                <p>
+                    Hic expedita vitae voluptate excepturi eaque provident nemo
+                    quis laudantium eius accusamus officia saepe ratione, dicta
+                    porro iusto sunt, distinctio debitis totam ad doloremque
+                    eligendi incidunt. Alias ut magnam ducimus placeat? Natus
+                    eos nostrum officia nemo quia, ullam dicta veritatis nobis?
+                    Dolores expedita officia repellat tempore asperiores, harum
+                    quae incidunt?
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default About;
